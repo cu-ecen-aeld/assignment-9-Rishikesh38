20cc552d8a4c56f124ce8db1f71ee0d6459cb46d
Rishi <rishi54@AESD.VM.myguest.virtualbox.org>
2023-09-24 16:29:35 -0600

    Made a change to kick the actions runner again

--- full-test.sh
+++ full-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script can be copied into your base directory for use with
-# automated testing using assignment-autotest.  It automates the
-# steps described in https://github.com/cu-ecen-5013/assignment-autotest/blob/master/README.md#running-tests
+# automated testing using assignment-autotest.  It automates the steps 
+# described in https://github.com/cu-ecen-5013/assignment-autotest/blob/master/README.md#running-tests
 set -e
 
 cd `dirname $0`
