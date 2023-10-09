75adfce7ae13e7c195e2460219f9a7ed26485308
Rishi <rishi54@AESD.VM.myguest.virtualbox.org>
2023-10-08 23:50:15 -0600

    locally works

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = e99eb034008789c984d3648626a1a902fa148a23
+AESD_ASSIGNMENTS_VERSION = ae8536a7ff45d63c7c0b9564b462ff1c6a9768f7
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
