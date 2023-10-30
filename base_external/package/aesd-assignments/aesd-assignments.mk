cc6a5b2dfc0600bdc52c46c13b8f3150fa743240
Rishikesh Sundaragiri <sundaragiririshi54@gmail.com>
2023-10-30 02:56:19 -0600

    Added aesd-char package

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = ae8536a7ff45d63c7c0b9564b462ff1c6a9768f7
+AESD_ASSIGNMENTS_VERSION = ca45b42e1ff944864e8351aee86aa87acd87cb00
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
